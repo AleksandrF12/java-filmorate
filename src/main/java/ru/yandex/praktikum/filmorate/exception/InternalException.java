04cf7e61129cb78807ce59fe4cae67d8652bd24f
AleksandrF12 <100619973+aleksandrf12@users.noreply.github.com>
2022-11-19 23:48:09 +0300

    Initial commit

---
+++ src/main/java/ru/yandex/praktikum/filmorate/exception/InternalException.java
@@ -0,0 +1,7 @@
+package ru.yandex.praktikum.filmorate.exception;
+
+public class InternalException extends RuntimeException {
+    public InternalException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ru/yandex/praktikum/filmorate/model/User.java
@@ -0,0 +1,28 @@
+package ru.yandex.praktikum.filmorate.model;
+
+import lombok.Data;
+
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+public class User {
+
+    private Set<Integer> friends = new HashSet<>();
+
+    @PositiveOrZero
+    private int id;
+    @NotBlank(message = "Отсутствует email")
+    @Email(message = "Некорректный email")
+    private final String email;
+    @NotNull(message = "Отсутствует логин")
+    @Pattern(regexp = "\\S+", message = "Логин содержит пробелы")
+    private final String login;
+    private String name;
+    @NotNull(message = "Не указана дата рождения")
+    @PastOrPresent(message = "Некорректная дата рождения")
+    private final LocalDate birthday;
+
+}
