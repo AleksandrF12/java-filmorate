1dd3ca7c46455a4e04cd331e1fc8d3abeeab724e
AleksandrF12 <100619973+aleksandrf12@users.noreply.github.com>
2022-11-19 23:48:09 +0300

    Initial commit

---
+++ src/main/java/ru/yandex/praktikum/filmorate/model/Film.java
@@ -0,0 +1,30 @@
+package ru.yandex.praktikum.filmorate.model;
+
+import lombok.Data;
+import ru.yandex.praktikum.filmorate.validate.BeginOfCinemaEra;
+
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+public class Film {
+
+    private Set<Integer> usersLikes = new HashSet<>();
+
+    @PositiveOrZero
+    private int id;
+    @NotBlank(message = "Не правильное название фильма")
+    private final String name;
+    @NotNull(message = "Отсутствует описание фильма")
+    @Size(max = 200, message = "слишком длинное описание, больше 200 символов")
+    private final String description;
+    @NotNull
+    @BeginOfCinemaEra
+    private final LocalDate releaseDate;
+    @Min(value = 1, message = "Неправильная продолжительность фильма")
+    @Positive
+    private final long duration;
+
+}
